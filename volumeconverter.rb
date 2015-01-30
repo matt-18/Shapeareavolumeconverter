79fdf83e29cebb7dc1c0657ad57b0c0ec6cdbeea
matthewengler <englerm18@gmail.com>
2015-01-30 12:20:18 -0500

    Initial Commit

---
+++ volumeconverter.rb
@@ -0,0 +1,19 @@
+"Welcome to the volume converter"
+
+puts "what is your length?"
+
+length = gets.to_f
+
+puts "what is your width"
+
+width = gets.to_f
+
+puts "what is your height"
+
+height = gets.to_f
+
+volume = length*width*height
+
+puts "Here is your volume"
+
+puts volume
